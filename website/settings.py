bb4452a15bd86ce4cf3706817af3f437420a49b4
E-wave112 <osagieiyayi09@gmail.com>
2020-08-23 15:28:15 +0100

    fifth

--- website/settings.py
+++ website/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['https://treadmillsite.herokuapp.com/', 'localhost']
 
