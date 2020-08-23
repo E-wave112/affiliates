745def650d13dee94b44f0ba1832e5bf3f6dd1bc
E-wave112 <osagieiyayi09@gmail.com>
2020-08-23 12:39:05 +0100

    third commit

--- website/settings.py
+++ website/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://treadmillsite.herokuapp.com/']
 
 
 # Application definition
@@ -130,6 +130,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 django_heroku.settings(locals())
