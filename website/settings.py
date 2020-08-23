7fef300daf1c709026fd861eed9320e4479606a2
E-wave112 <osagieiyayi09@gmail.com>
2020-08-23 12:10:38 +0100

    second commit

--- website/settings.py
+++ website/settings.py
@@ -9,11 +9,13 @@ https://docs.djangoproject.com/en/3.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
-
 import os
-import django_heroku
-import dj_database-url
 
+import django_heroku
+
+import dj_database_url
+
+from decouple import config
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -22,10 +24,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'yg&w0ix5#7c&-24nhkzva5d5h(!8kf6vi#38&lzwdk)u!r_dps'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
